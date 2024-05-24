2252002b9479c887b0a431621ef36efa73d9869a
YashGupt29 <yashnitagartala8@gmail.com>
2024-05-24 18:11:51 +0530

    fixes

--- app/(landingPage)/(login)/login/page.tsx
+++ app/(landingPage)/(login)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginCard } from "../_components/loginCard";
 
 const LoginPage = () => {
   return (
-    <div className="h-[85vh] lg:h-[870px] bg-gradient-to-b from-[#255CB8] to-sky-100 flex items-center justify-center ">
+    <div className="h-[85vh] lg:h-[90vh] bg-gradient-to-b from-[#255CB8] to-sky-100 flex items-center justify-center ">
       <LoginCard />
     </div>
   );
